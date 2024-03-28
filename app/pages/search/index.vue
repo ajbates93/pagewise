5e2e52a83e73317a29acb8c56954423925b51ac7
Alex Bates <ajbates93@gmail.com>
2024-03-28 11:17:19 +0000

    feat: add debouncing of search input

--- app/pages/search/index.vue
+++ app/pages/search/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="search" class="flex justify-between items-center">
+  <div id="search">
     <LayoutDeviceHeightContainer class="w-full">
       <div></div>
       <div class="flex items-center gap-10 w-full">
@@ -12,43 +12,67 @@
         <div>
           <UInput
             size="xl"
+            :loading="isLoading"
+            icon="i-heroicons-magnifying-glass-20-solid"
             class="w-full min-w-[500px]"
             placeholder="e.g. The Lord of the Rings"
             v-model="searchInput"
-            @input="handleSearchInput"
           />
         </div>
       </div>
       <div></div>
     </LayoutDeviceHeightContainer>
-    <div id="results" v-for="(book, idx) in searchResults">
-      {{ book.title }}
-    </div>
+    <LayoutDeviceHeightContainer>
+      <div class="p-20">
+        <h2 class="text-3xl">Results</h2>
+        <div id="results" v-for="(book, idx) in searchResults">
+          <div>
+            <h3>{{ book.volumeInfo.title }}</h3>
+            <p class="text-gray-400">
+              {{ book.volumeInfo.authors.join(", ") }}
+            </p>
+            <p>{{ book.volumeInfo.description }}</p>
+          </div>
+        </div>
+      </div>
+    </LayoutDeviceHeightContainer>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { BaseAPIResponse } from "~/types/API";
+import type { BaseAPIResponse, APIBookResponse } from "~/types/API";
+import { refDebounced } from "@vueuse/core";
 
-type BookAPIResponse = {
-  title: string;
-};
-
+const isLoading = ref<boolean>(false);
 const searchInput = ref<string>("");
-const searchResults = ref<BookAPIResponse[]>();
+const searchInputDebounced = refDebounced(searchInput, 500);
+const searchResults = ref<APIBookResponse[]>();
 
 const handleSearchInput = async () => {
-  if (searchInput.value.length > 3) {
-    // Call search API
-    const { data, pending } = await useAPIFetch<BaseAPIResponse>(
-      `/search?q=${searchInput.value}`
-    );
+  try {
+    if (searchInput.value.length > 3) {
+      // Call search API
+      const { data, pending } = await useAPIFetch<BaseAPIResponse>(
+        `/search?q=${searchInput.value}`
+      );
 
-    if (data.value && data.value.success) {
-      searchResults.value = data.value.data as BookAPIResponse[];
+      if (data.value && data.value.success) {
+        searchResults.value = data.value.data as APIBookResponse[];
+      }
+    } else if (searchInput.value.length === 0) {
+      searchResults.value = [];
     }
+  } catch (e: any) {
+    console.error(e);
+  } finally {
+    isLoading.value = false;
   }
 };
+
+watch(searchInputDebounced, handleSearchInput);
+watch(searchInput, () => {
+  if (searchInput.value !== searchInputDebounced.value) isLoading.value = true;
+});
 </script>
 
 <style></style>
--- app/types/API.ts
+++ app/types/API.ts
@@ -3,3 +3,21 @@ export type BaseAPIResponse = {
   message: string;
   data: any;
 };
+
+export type APIBookResponse = {
+  volumeInfo: {
+    authors: string[];
+    title: string;
+    description: string;
+    imageLinks: {
+      thumbnail: string;
+    };
+    categories: string[];
+    industryIndentifiers: {
+      type: string;
+      identifier: string;
+    };
+    pageCount: number;
+    publishedDate: string;
+  };
+};
